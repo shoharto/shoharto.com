d0a2423d8d31e90245a17661dd2f42160635d200
shoharto <bayes@graduate.utm.my>
2020-06-15 20:02:19 +0800

    2nd commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -70,7 +70,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
+    fetch('https://api.github.com/repos/shoharto/shoharto.com')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -102,10 +102,10 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://github.com/shoharto/shoharto.com"
           target="_blank"
           rel="nofollow noopener noreferrer">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+          <div>Designed &amp; Built by Bayes Ahmed Shoharto</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <StyledGitHubInfo>
